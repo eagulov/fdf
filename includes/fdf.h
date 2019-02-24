079349f34b9d2607d03c75394fe1f5c53ae4205f
eagulov <45741029+eagulov@users.noreply.github.com>
2019-02-23 21:58:43 -0800

    Add files via upload

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: eagulov <eagulov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 17:39:04 by eagulov           #+#    #+#             */
-/*   Updated: 2019/02/22 09:30:03 by eagulov          ###   ########.fr       */
+/*   Updated: 2019/02/23 20:06:35 by eagulov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,15 @@ typedef struct	s_point
 
 typedef	struct	s_map
 {
+	int		sx;
+	int		sy;
+	int		d;
+	int		d1;
+	int		d2;
+	int		i;
+	int		dx;
+	int		dy;
+	int		color;
 	void	*mlx_ptr;
 	void	*win_ptr;
 	int		height;
@@ -38,9 +47,15 @@ typedef	struct	s_map
 	int		y_now;
 }				t_map;
 
-void	read_map(t_map *map, int fd);
+void    trans_coords(int z, t_point *pnt, t_map *map, int i);
+void    coords(t_point *pnt, t_map *map, char *line);
+void    init_map(t_map *map, int x, int y);
+void	keep_draw(t_point *pnt, int count, t_map *map);
+void	draw_y(t_point *pnt, int count, t_map *map);
+void    get_map_info(t_map *map, int fd);
+void	main_helper(char *file, t_map *map, int fd);
 int		press(int key, void *param);
-
-
+void    connect_x(t_point *pnt, int i, t_map *map);
+void	connect_y(t_point *pnt, t_map *map);
 
 #endif
