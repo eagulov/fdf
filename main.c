8b0a81dda63e67159b73e0ec98a2eb05fa810ad2
eagulov <45741029+eagulov@users.noreply.github.com>
2019-03-08 12:37:56 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eagulov <eagulov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 17:24:34 by eagulov           #+#    #+#             */
-/*   Updated: 2019/03/08 12:28:13 by eagulov          ###   ########.fr       */
+/*   Updated: 2019/03/08 12:37:31 by eagulov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,13 +36,13 @@ int	main(int argc, char **argv)
 
 	if ((argc != 2) || (fd = open(argv[1], O_RDONLY) < 0))
 	{
-		write(1, "'Expected file name or something wrong with ./fdf'\n", 52);
+		write(1, "Usade: maps/.fdf or smtg wrong with ./fdf\n", 42);
 		exit(0);
 	}
 	read_fdf(fd, &map);
 	if ((mlx = init(argv[1])) == NULL)
 	{
-		write(1, "Error: 'mlx couldn't initialize'\n", 34);
+		write(1, "Error: mlx couldn't initialize\n", 32);
 		exit(0);
 	}
 	mlx->map = map;
