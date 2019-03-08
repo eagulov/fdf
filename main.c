6b706219ed4be789b2ba7e82e34f4941e2d76dbb
eagulov <45741029+eagulov@users.noreply.github.com>
2019-03-08 12:31:29 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: eagulov <eagulov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/02 17:24:34 by eagulov           #+#    #+#             */
-/*   Updated: 2019/03/06 20:34:28 by eagulov          ###   ########.fr       */
+/*   Updated: 2019/03/08 12:28:13 by eagulov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,17 @@ int	main(int argc, char **argv)
 	t_mlx	*mlx;
 	int		fd;
 
-	if (argc != 2)
-		write(1, "Usage: ./fdf 'Expected file name'\n", 35);
-	if ((fd = open(argv[1], O_RDONLY)) < 0)
-		write(1, "Error: 'fdf is wrong'\n", 23);
+	if ((argc != 2) || (fd = open(argv[1], O_RDONLY) < 0))
+	{
+		write(1, "'Expected file name or something wrong with ./fdf'\n", 52);
+		exit(0);
+	}
 	read_fdf(fd, &map);
 	if ((mlx = init(argv[1])) == NULL)
+	{
 		write(1, "Error: 'mlx couldn't initialize'\n", 34);
+		exit(0);
+	}
 	mlx->map = map;
 	perform(mlx);
 	mlx_key_hook(mlx->window, press_button, mlx);
